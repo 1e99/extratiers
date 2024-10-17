f19381d112a9339f1db11a40a0ca809846b15921
1e99 <noemail@noemail>
2024-10-17 18:03:13 +0200

    add items.lua file

--- data.lua
+++ data.lua
@@ -1,11 +1,1 @@
-data:extend({
-  {
-    type = "item",
-    name = "dino-nugget",
-    stack_size = 2000,
-    icon = "__extratiers__/icons/dino-nugget.png",
-    icon_size = 300,
-    subgroup = "raw-resource",
-    place_result = "assembling-machine-1",
-  },
-})
+require "data/items.lua"
