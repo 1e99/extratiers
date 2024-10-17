cae645c012df007f41de661d36fbac83d749417c
1e99 <noemail@noemail>
2024-10-17 17:59:58 +0200

    add dino-nugget item

---
+++ data.lua
@@ -0,0 +1,11 @@
+data:extend({
+  {
+    type = "item",
+    name = "dino-nugget",
+    stack_size = 2000,
+    icon = "__extratiers__/icons/dino-nugget.png",
+    icon_size = 300,
+    subgroup = "raw-resource",
+    place_result = "assembling-machine-1",
+  },
+})
